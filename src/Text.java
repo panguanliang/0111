db2714f67947fc6f44baf4009c052bcb783ba0ff
Administrator <1846201952@qq.com>
2021-01-11 19:41:59 +0800

    创建主类

--- src/Text.java
+++ src/Text.java
@@ -1,5 +1,5 @@
-public class getpassword {
-    public static void main(String[] args) {
-        System.out.println("Hello");
+public class Text(){
+public static void main(String[]args){
+
     }
 }
